011ffa0b65f20fdab62abc13dae197b5c7d7b8ae
Julio Amaya <atpjulio@yahoo.es>
2018-11-03 21:04:41 -0500

    Test on invoices

--- resources/views/invoice/fields2.blade.php
+++ resources/views/invoice/fields2.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group @if($errors->has('authorization_code')) has-error @endif">
     {!! Form::label('authorization_code', 'Autorización', ['class' => 'control-label']) !!}
-    {!! Form::text('authorization_code', old('authorization_code', isset($invoice) ? $invoice->authorization_code : ''), ['class' => 'form-control underlined', 'id' => 'authorization_code']) !!}
+    {!! Form::text('authorization_code', old('authorization_code', isset($invoice) ? $invoice->authorization_code : ''), ['class' => 'form-control underlined', 'readonly', 'id' => 'authorization_code']) !!}
 </div>
 <div class="form-group @if($errors->has('total_days')) has-error @endif">
     {!! Form::label('total_days', 'Total de días', ['class' => 'control-label']) !!}
@@ -8,5 +8,5 @@
 </div>
 <div class="form-group @if($errors->has('total')) has-error @endif">
     {!! Form::label('total', 'Monto', ['class' => 'control-label']) !!}
-    {!! Form::number('total', old('total', isset($invoice) ? $invoice->total : ''), ['class' => 'form-control underlined', 'placeholder' => 'Valor total de la factura', 'min' => '0', 'id' => 'total']) !!}
+    {!! Form::number('total', old('total', isset($invoice) ? $invoice->total : ''), ['class' => 'form-control underlined', 'placeholder' => 'Valor total de la factura', 'min' => '0', 'readonly', 'id' => 'total']) !!}
 </div>
