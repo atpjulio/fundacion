d94ba1bb96f2389d90247f2eae3713dee896a7df
Julio Amaya <atpjulio@yahoo.es>
2018-11-03 21:55:30 -0500

    Changing order for index table

--- config/constants.php
+++ config/constants.php
@@ -9,7 +9,7 @@ $myConstants = [
         'admin' => 'fundacasahogar_elmilagro@hotmail.com',
         'admin2' => 'zuleima0326@gmail.com'
     ],
-    'stylesVersion' => '1.015',
+    'stylesVersion' => '1.016',
     'productImages' => '/img/products/',
     'usersImages' => '/img/users/',
     'companiesImages' => '/img/companies/',
--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -69,5 +69,5 @@
 @push('scripts')
     <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('js/dataTables.bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/general/table.js') }}"></script>
+    <script src="{{ asset('js/general/table.js').'?version='.config('constants.stylesVersion') }}"></script>
 @endpush
