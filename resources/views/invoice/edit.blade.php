0030947bc22494e801be8624549181d8e8b84c9c
Julio Amaya <atpjulio@yahoo.es>
2018-11-06 21:50:59 -0500

    Fixes on invoices

--- resources/views/invoice/edit.blade.php
+++ resources/views/invoice/edit.blade.php
@@ -56,19 +56,92 @@
                 "oSearch": {"sSearch": "{{ $invoice->authorization_code }}"}
             });
             $('#myTable').on('click','.btn-success', function() {
-                // console.log($(this).parent().parent().find('td')[5].outerText);
-                $('#total_days').val($(this).parent().parent().find('td')[5].outerText);
-                $('#selected_price').val($(this).parent().find('input')[0].value);
-                $('#total').val($('#selected_price').val() * $('#total_days').val());
-                $('#authorization_code').val($(this).parent().parent().find('td').first()[0].outerText);
-                $('html, body').animate({
-                    scrollTop: $('#beginning').offset().top
-                }, 300, function(){
-                    window.location.href = '#beginning';
-                });
+                days = parseInt($(this).parent().parent().find('td')[5].outerText);
+                if ($('#multiple').is(":checked")) {
+                    console.log($("#multiple_table tr").length);
+                    console.log($("#multiple_table tr:nth-child(1)").find('td input')[0].value.length);
+                    if ($("#multiple_table tr:nth-child(1)").find('td input')[0].value.length > 0) {
+                        $("#multiple_table").append(
+                            '<tr><td><input type="text" id="multiple_codes" name="multiple_codes[]" value="' + $(this).parent().parent().find('td').first()[0].outerText.trim() + '" class="form-control" placeholder="Número de autorización" />'
+                            + '</td><td><input type="number" id="multiple_days" name="multiple_days[]" value="' + days + '" class="form-control" placeholder="Total de días" min="0"/>'
+                            + '</td><td><input type="number" id="multiple_totals" name="multiple_totals[]" value="' + ($(this).parent().find('input')[0].value * days) + '" class="form-control" placeholder="Valor total" min="0"/>'
+                            + '</td><td><a href="javascript:void(0);" class="removeRow btn btn-oval btn-danger">Quitar</a></td></tr>'
+                        );
+                    } else {
+                        $("#multiple_table tr:last").remove();
+                        $("#multiple_table").append(
+                            '<tr><td><input type="text" id="multiple_codes" name="multiple_codes[]" value="' + $(this).parent().parent().find('td').first()[0].outerText.trim() + '" class="form-control" placeholder="Número de autorizaciónnnnn" />'
+                            + '</td><td><input type="number" id="multiple_days" name="multiple_days[]" value="' + days + '" class="form-control" placeholder="Total de días" min="0"/>'
+                            + '</td><td><input type="number" id="multiple_totals" name="multiple_totals[]" value="' + ($(this).parent().find('input')[0].value * days) + '" class="form-control" placeholder="Valor total" min="0"/>'
+                            + '</td><td><a href="javascript:void(0);" class="addRow btn btn-oval btn-success">Añadir</a></td></tr>'
+                        );
+                    }
+
+                    $('#alertTable').css('display', 'none');
+                    $('#tableMessage').html('');              
+
+                    $('html, body').animate({
+                        scrollTop: $('#multiple_table').offset().top
+                    }, 300, function(){
+                        window.location.href = '#multiple_table';
+                    });
+                } else {
+                    $('#total_days').val(days);
+                    $('#selected_price').val($(this).parent().find('input')[0].value);
+                    $('#total').val($('#selected_price').val() * $('#total_days').val());
+                    //$('#total').val($('#selected_price').val() * $('#total_days').val() + parseInt($('#total').val()));
+                    $('#authorization_code').val($(this).parent().parent().find('td').first()[0].outerText.trim());
+                    //$('#authorization_code').val($(this).parent().parent().find('td').first()[0].outerText.trim() + "," + $('#authorization_code').val());
+
+                    $('html, body').animate({
+                        scrollTop: $('#beginning').offset().top
+                    }, 300, function(){
+                        window.location.href = '#beginning';
+                    });            
+                }
             });
             $('#total_days').on('change', function (e) {
                 $('#total').val($('#selected_price').val() * $('#total_days').val());
+            });
+            $('#multiple').on('change', function (e) {
+                if ($('#multiple').is(":checked")) {
+                    $('#multiple').val("1");
+                    $('#multiple_card').css('display', 'block');
+                    $('#multiple_card').addClass('animated fadeIn');
+                    $('#authorization_code').val('');
+                    $('#total_days').val('');
+                    $('#total').val('');
+                } else {
+                    $('#multiple_card').css('display', 'none');
+                    $('#multiple').val("0");
+                }
+            });
+            $("#multiple_table").on('click','.addRow', function() {
+                if ($('#multiple_codes').val().length > 0 && $('#multiple_days').val() > 0 && $('#multiple_totals').val() > 0) {
+                    $("#multiple_table").append(
+                        '<tr><td><input type="text" id="multiple_codes" name="multiple_codes[]" class="form-control" placeholder="Número de autorización" value=""/>'
+                        + '</td><td><input type="number" id="multiple_days" name="multiple_days[]" class="form-control" placeholder="Total de días" min="0" value=""/>'
+                        + '</td><td><input type="number" id="multiple_totals" name="multiple_totals[]" class="form-control" placeholder="Valor total" min="0" value=""/>'
+                        + '</td><td><a href="javascript:void(0);" class="removeRow btn btn-oval btn-danger">Quitar</a></td></tr>'
+                    );
+                    $('#alertTable').css('display', 'none');
+                    $('#tableMessage').html('');              
+                } else {
+                    if ($('#multiple_codes').val().length == 0) {
+                        $('#tableMessage').html('Número de autorización inválido');              
+                        $('#alertTable').css('display', 'block');
+                    } else if ($('#multiple_days').val().length == 0) {
+                        $('#tableMessage').html('Número de días inválido');              
+                        $('#alertTable').css('display', 'block');                        
+                    } else if ($('#multiple_totals').val().length == 0) {
+                        $('#tableMessage').html('Monto de factura inválido');              
+                        $('#alertTable').css('display', 'block');                        
+                    }
+                }
+
+            });
+            $("#multiple_table").on('click','.removeRow', function() {
+                $(this).parent().parent().remove();
             });
         } );
     </script>
