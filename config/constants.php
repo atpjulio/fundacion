54207d42f31d53d54ed11a52b84e42d90f94d337
Julio Amaya <atpjulio@yahoo.es>
2018-11-16 21:40:01 -0500

    JS Version

--- config/constants.php
+++ config/constants.php
@@ -10,7 +10,7 @@ $myConstants = [
         'admin2' => 'zuleima0326@gmail.com'
     ],
     'pagination' => 20,
-    'stylesVersion' => '1.067',
+    'stylesVersion' => '1.069',
     'productImages' => '/img/products/',
     'usersImages' => '/img/users/',
     'companiesImages' => '/img/companies/',
