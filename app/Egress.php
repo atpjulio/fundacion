91ec1b9cd32e2a751fb49babfde2d21c6bca6e9e
Julio Amaya <atpjulio@yahoo.es>
2019-01-29 11:15:31 -0500

    Order test

--- app/Egress.php
+++ app/Egress.php
@@ -134,7 +134,8 @@ class Egress extends Model
         }
 
         return $query
-            ->orderBy('egresses.created_at', 'DESC')
+            ->orderByRaw('SUBSTRING(egresses.created_at, 0, 7)', 'DESC')
+            //->orderBy('egresses.created_at', 'DESC')
             ->orderBy('egresses.counter', 'DESC')
             ->paginate(config('constants.pagination'));
     }
