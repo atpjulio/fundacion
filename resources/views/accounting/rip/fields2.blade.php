f71a98e05139befa5ab5463507867bb5d28212f7
Julio Amaya <atpjulio@yahoo.es>
2019-06-15 11:14:37 -0500

    Small changes on RIPS verbiage

--- resources/views/accounting/rip/fields2.blade.php
+++ resources/views/accounting/rip/fields2.blade.php
@@ -8,11 +8,11 @@
 	    {!! Form::label('hint', 'Facturas disponibles para EPS', ['class' => 'control-label']) !!}
 	    @if ($invoicesAmount > 0) 
 		    <div class="alert alert-dark" role="alert">
-	  			Total: {!! $invoicesAmount !!} factura(s) disponible(s) para el rango de fecha seleccionado
+	  			Total: {!! $invoicesAmount !!} factura(s) disponible(s) para el rango seleccionado
 			</div>
 		@else
 		    <div class="alert alert-warning" role="alert" style="background-color: #fff3cd; color: #856404;">
-	  			No hay facturas disponibles para el rango de fecha seleccionado
+	  			No hay facturas disponibles para el rango seleccionado
 			</div>
 		@endif
 	</div>
